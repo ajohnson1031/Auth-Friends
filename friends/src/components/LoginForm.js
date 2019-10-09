aaac65964cf0ecd0bc273916b33b57052d64c5e4
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-10-08 22:47:12 -0400

    added router, form

---
+++ friends/src/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginForm = props => {
+  return (
+    <form>
+      <input type="text" />
+      <input type="text" />
+      <button type="submit"></button>
+    </form>
+  );
+};
+
+export default LoginForm;
